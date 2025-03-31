11ebc1004d97d34a916c357006048b573dd8701e
Wallace027Dev <wallaceofc@hotmail.com>
2025-03-31 11:38:00 -0300

    adicionando renderizacao condicinoal no campo de texto

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const formulario = document.getElementById("contateNosForm");
-
+const mensagem = document.getElementById("mensagem");
+const desenvolvimentoInputs = "input[name='desenvolvimento']:checked";
 document.getElementById("whatsapp").addEventListener("input", function () {
   this.value = this.value.replace(/\D/g, "");
 });
 
+
+document
+  .querySelector(".desenvolvimento")
+  .addEventListener("click", mostrarCampoDeMensagem);
+
 formulario.addEventListener("submit", manipuladorDeEnvio);
+
+function mostrarCampoDeMensagem() {
+  const inputSelecionado = document.querySelector(desenvolvimentoInputs);
+  const valorSelecionado = inputSelecionado?.value;
+  const container = document.querySelector(".mensagem__container");
+
+  container.style.display = valorSelecionado === "outro" ? "block" : "none";
+  console.log(container.style.display);
+}
 
 function manipuladorDeEnvio(event) {
   event.preventDefault();
@@ -17,8 +32,8 @@ function manipuladorDeEnvio(event) {
   const sobrenome = document.getElementById("sobrenome").value.trim();
   const email = document.getElementById("email").value.trim();
   const whatsapp = document.getElementById("whatsapp").value.trim();
-  const desenvolvimento = document.querySelector("input[name='desenvolvimento']:checked")?.value;
-  const mensagem = document.getElementById("mensagem").value.trim();
+  const desenvolvimento = document.querySelector(desenvolvimentoInputs)?.value;
+  const mensagemValor = mensagem.value.trim();
 
   const servico = {
     nome,
@@ -26,7 +41,7 @@ function manipuladorDeEnvio(event) {
     email,
     whatsapp,
     desenvolvimento,
-    mensagem,
+    mensagemValor
   };
 
   console.log(servico);
